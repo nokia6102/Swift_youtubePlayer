c17596bc76f0fee8aa62d3518aadf4677c96b046
nokia6102 <nokia6102@gmail.com>
2017-08-06 11:26:28 +0800

    讀取playtime時間軸

--- Swift_youPlayer/Swift_youPlayer/ViewController.swift
+++ Swift_youPlayer/Swift_youPlayer/ViewController.swift
@@ -1,28 +1,23 @@
-//
-//  ViewController.swift
-//  Swift_youPlayer
-//
-//  Created by Leon Cheng on 2017/8/6.
-//  Copyright © 2017年 Leon Cheng. All rights reserved.
-//
 
 import UIKit
 import youtube_ios_player_helper
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,YTPlayerViewDelegate {
   @IBOutlet weak var playerView: YTPlayerView!
 
+  @IBOutlet weak var lblTime: UILabel!
   override func viewDidLoad()
   {
     super.viewDidLoad()
     self.playerView.load(withVideoId: "yYIBb81ocvg", playerVars: ["playsinline": 1])
+    self.playerView.delegate = self
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  //YTPLayerViewDelegate
+  
+  func playerView(_ playerView: YTPlayerView, didPlayTime playTime: Float) {
+      print ("playTime :\(playTime)")
+     lblTime.text = "\(playTime)"
   }
-
-
 }
 
