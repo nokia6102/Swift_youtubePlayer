8e80ba717e6b9fb058864c77f459a2347f13f4d0
nokia6102 <nokia6102@gmail.com>
2017-08-06 10:00:46 +0800

    加入pod及安裝pod "youtube-ios-player-helper", "~> 0.1.4"

---
+++ Swift_youPlayer/Pods/Target Support Files/Pods-Swift_youPlayer/Pods-Swift_youPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift_youPlayer : NSObject
+@end
+@implementation PodsDummy_Pods_Swift_youPlayer
+@end
---
+++ Swift_youPlayer/Pods/Target Support Files/Pods-Swift_youPlayerTests/Pods-Swift_youPlayerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift_youPlayerTests : NSObject
+@end
+@implementation PodsDummy_Pods_Swift_youPlayerTests
+@end
