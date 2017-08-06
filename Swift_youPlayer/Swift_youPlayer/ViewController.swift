1c8ff3d21a1c9be0b8097513f3528e05900e0be1
nokia6102 <nokia6102@gmail.com>
2017-08-06 09:58:02 +0800

    第1次空專案

---
+++ Swift_youPlayer/Swift_youPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_youPlayer
+//
+//  Created by Leon Cheng on 2017/8/6.
+//  Copyright © 2017年 Leon Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
