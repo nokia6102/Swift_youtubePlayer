8ed354a52ebbba81ff82addec20664f8b094f5a1
nokia6102 <nokia6102@gmail.com>
2017-08-06 10:17:37 +0800

    加入MyView客製TYPlayer

---
+++ Swift_youPlayer/MyView.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+import youtube_ios_player_helper
+
+class MyView: YTPlayerView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Swift_youPlayer/Swift_youPlayer/ViewController.swift
+++ Swift_youPlayer/Swift_youPlayer/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import youtube_ios_player_helper
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
   }
 
   override func didReceiveMemoryWarning() {
