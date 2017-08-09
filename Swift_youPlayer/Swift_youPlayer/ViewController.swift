710ab8f2754c14b1d3ab4cfbf375c16ff7ceffe6
nokia6102 <nokia6102@gmail.com>
2017-08-10 00:04:00 +0800

    自動播放到停止

--- Swift_youPlayer/Swift_youPlayer/ViewController.swift
+++ Swift_youPlayer/Swift_youPlayer/ViewController.swift
@@ -4,20 +4,43 @@ import youtube_ios_player_helper
 
 class ViewController: UIViewController,YTPlayerViewDelegate {
   @IBOutlet weak var playerView: YTPlayerView!
-
+  let playerVars = ["playsinline": 1]
+  
   @IBOutlet weak var lblTime: UILabel!
   override func viewDidLoad()
   {
     super.viewDidLoad()
-    self.playerView.load(withVideoId: "yYIBb81ocvg", playerVars: ["playsinline": 1])
+   
+    self.playerView.load(withVideoId: "S9xzKII9yRw", playerVars: playerVars)
     self.playerView.delegate = self
+    
   }
 
-  //YTPLayerViewDelegate
+  @IBAction func btnLoadList(_ sender: UIButton)
+  {
+        self.playerView.load(withPlaylistId: "PLNimSq2k6r46NtbwbHLjl9pjVidMPiTQ7", playerVars: playerVars)
+  }
   
+  @IBAction func btnPrev(_ sender: Any)
+  {
+    self.playerView.previousVideo()
+  }
+  
+  @IBAction func btnNext(_ sender: UIButton)
+  {
+    self.playerView.nextVideo()
+  }
+  
+  //YTPLayerViewDelegate
   func playerView(_ playerView: YTPlayerView, didPlayTime playTime: Float) {
       print ("playTime :\(playTime)")
      lblTime.text = "\(playTime)"
+     if Int(playTime) == Int (playerView.duration())
+     {
+      playerView.stopVideo()
+  
+      
+    }
   }
 }
 
